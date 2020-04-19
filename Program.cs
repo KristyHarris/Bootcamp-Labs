6501a0e58358d8ccba6699b6df5531c6c89359a9
Kristy <harrisk26@gmail.com>
2020-04-19 16:31:50 -0400

    adds lab 2

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Grand Circus' Room Calculator!");
+            Console.WriteLine("");
+            string response;
+
+            do
+            {
+                Console.Write("Enter Length: ");
+                var length = float.Parse(Console.ReadLine());
+
+                Console.Write("Enter Width: ");
+                var width = float.Parse(Console.ReadLine());
+
+                var area = (length * width);
+                Console.WriteLine("Area: " + area);
+
+                var perimeter = (length * 2) + (width * 2);
+                Console.WriteLine("Perimeter: " + perimeter);
+
+                Console.WriteLine("Continue? y/n");
+                response = Console.ReadLine();
+            } while (response == "y" || response == "Y");
+
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
